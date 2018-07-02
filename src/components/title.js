32d824f4710c01ca37440bd90eb8926c5b096caa
Mark <m.d.maeske@gmail.com>
2018-07-02 15:39:41 +0200

    initial commit

---
+++ src/components/title.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'gatsby-link'
+
+class HeaderTitle extends React.Component {
+    render() {
+        const siteTitle = this.props.siteTitle
+        return (
+            <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '1.45rem 1.0875rem',
+          }}
+        >
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+        </div>
+        )
+    }
+};
+
+export default HeaderTitle;
