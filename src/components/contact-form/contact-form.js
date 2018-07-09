ce9770aa7b6bdcf65acb43e305fad190afaa4030
Martin Köhn <m.koehn@mip-consult.de>
2018-07-09 10:57:34 +0000

    update from gitlab

--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -7,63 +7,38 @@ class ContactForm extends Component {
         this.state = { formSend: false};
     }
 
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-      };
-    
-      handleSubmit = e => {
-        e.preventDefault();
-        const form = e.target;
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({
-            "form-name": form.getAttribute("name"),
-            ...this.state
-          })
-        })
-          .then(() => this.setState({formSend: true}))
-          .catch(error => alert(error));
-      };
-
     render() {
 
         if (!this.state.formSend) {
             return (
                 <div className="contact">
-                    <form name="contact"
-                            method="post"
-                            action="/thanks/"
-                            data-netlify="true"
-                            data-netlify-honeypot="bot-field"
-                            onSubmit={this.handleSubmit}
-                    >
+                    <form form name="contact" method="POST" netlify >
                         <input type="hidden" name="form-name" value="contact" />
                         <p hidden>
                         <label>
                             Don’t fill this out:{" "}
-                            <input name="bot-field" onChange={this.handleChange} />
+                            <input name="bot-field"/>
                         </label>
                         </p>
                         <p>
                             <label htmlFor="name">Name</label>
-                            <input type="text" name="name" id="name" onChange={this.handleChange} />
+                            <input type="text" name="name" id="name"/>
                         </p>
                         <p>
                             <label htmlFor="email">Email</label>
-                            <input type="text" name="email" id="email" onChange={this.handleChange} />
+                            <input type="text" name="email" id="email"/>
                         </p>
                         <p>
                             <label htmlFor="firm">Firma</label>
-                            <input type="text" name="firm" id="firm" onChange={this.handleChange} />
+                            <input type="text" name="firm" id="firm"/>
                         </p>
                         <p>
                             <label htmlFor="phone">Telefon</label>
-                            <input type="text" name="phone" id="phone" onChange={this.handleChange}/>
+                            <input type="text" name="phone" id="phone"/>
                         </p>
                         <p className="full-width">
                             <label htmlFor="message">Ihre Nachricht an uns</label>
-                            <textarea name="message" id="message" rows="12" onChange={this.handleChange} />
+                            <textarea name="message" id="message" rows="12"/>
                         </p>
                         <p className="full-width">
                             <input type="submit" value="Nachricht absenden" />
