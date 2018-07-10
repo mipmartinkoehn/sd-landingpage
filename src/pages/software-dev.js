a5ad78db31e561d0ef7b301669e4771fe47b3829
Martin Köhn <m.koehn@mip-consult.de>
2018-07-10 11:38:09 +0000

    update from gitlab

--- src/pages/software-dev.js
+++ src/pages/software-dev.js
@@ -1,8 +1,9 @@
 import React from 'react'
+
 import Greybox from '../components/grey-box/grey-box.module';
 import BlueBox from '../components/blue-box/blue-box';
-
 import Whitebox from '../components/white-box/white-box.module';
+
 import CodeWhite from '../images/code-icon_weiß.png';
 import RocketWhite from '../images/raketen-icon_weiß.png';
 import ConceptWhite from '../images/konzept-icon_weiß.png';
@@ -61,22 +62,22 @@ const SoftwareDev = () => (
     <h2>Technologien</h2>
     <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
       <div style={{margin: '0 1rem'}}>
-        <img src={Angular}   style={{height: '200px'}}/>
+        <a href="https://angular.io/"><img src={Angular}   style={{height: '200px'}}/></a>
       </div>
       <div style={{margin: '2rem 1rem'}}>
-        <img src={Firebase}   style={{maxHeight: '200px'}}/>
+      <a href="https://firebase.google.com/"><img src={Firebase}   style={{maxHeight: '200px'}}/></a>
       </div>
       <div style={{margin: '0 1rem'}}>
-        <img src={Golang}   style={{height: '200px'}}/>
+      <a href="https://golang.org/"><img src={Golang}   style={{height: '200px'}}/></a>
       </div>
       <div style={{margin: '0 1rem'}}>
-        <img src={NodeJs}   style={{height: '200px'}}/>
+      <a href="https://nodejs.org/"><img src={NodeJs}   style={{height: '200px'}}/></a>
       </div>
       <div style={{margin: '0 1rem'}}>
-        <img src={Loopback}   style={{maxHeight: '200px'}}/>
+      <a href="https://loopback.io/"><img src={Loopback}   style={{maxHeight: '200px'}}/></a>
       </div>
       <div style={{margin: '0 1rem'}}>
-        <img src={MongoDB}   style={{maxHeight: '100px'}}/>
+      <a href="https://www.mongodb.com/pressroom/"><img src={MongoDB}   style={{maxHeight: '100px'}}/></a>
       </div>
     </div>
   </BlueBox>
