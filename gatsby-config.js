88bb3825c7e8602a0c5d2ec3cacdb7d2b18ce8ba
Martin Köhn <m.koehn@mip-consult.de>
2018-07-11 13:48:17 +0000

    update from gitlab

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,16 @@ module.exports = {
     `gatsby-plugin-react-next`,
     {
       resolve: "gatsby-plugin-no-sourcemaps",
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "YOUR_GOOGLE_TAGMANAGER_ID",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+      },
+    },
   ],
 }
