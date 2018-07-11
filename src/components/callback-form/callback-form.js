72754c8be152b9b82b188c168460277d3fd7d39f
Martin Köhn <m.koehn@mip-consult.de>
2018-07-11 12:29:08 +0000

    update from gitlab

--- src/components/callback-form/callback-form.js
+++ src/components/callback-form/callback-form.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { navigateTo } from 'gatsby-link';
 import './callback-form.css';
-import Form from '../form/forms.module';
+import Recaptcha from "react-google-recaptcha";
 import '../form/forms.css';
+
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 
 const encode = (data) => {
     return Object.keys(data)
@@ -14,59 +16,71 @@ const encode = (data) => {
 class CallbackForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { formSend: false, name: "", email: "", message: "", phone: "", firm: "" };
+        this.state = { };
     }
 
-    handleSubmit = e => {
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+      };
+    
+      handleRecaptcha = value => {
+        this.setState({ "g-recaptcha-response": value });
+      };
+    
+      handleSubmit = e => {
+        e.preventDefault();
+        if (!this.state['gg-recaptcha-response']) {
+            alert('recaptcha nicht bestätigt')
+            return;
+        }
+        const form = e.target;
         fetch("/", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "callback", ...this.state })
+          body: encode({
+            "form-name": form.getAttribute("name"),
+            ...this.state
+          })
         })
-          .then(() => navigateTo('/contact-success'))
-          .catch(error => console.log(error));
-  
-        e.preventDefault();
+          .then(() => navigateTo(form.getAttribute("action")))
+          .catch(error => alert(error));
       };
-  
-    handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render() {
-        const { name, firm, email, phone, message } = this.state;
         return (
             <div className="contact">
-                <form name="callback" method="POST" action="/contact-success" netlify-recaptcha netlify="true" >
-                {/* netlify-honeypot="bot-field" */}
+                <form name="callback" method="post"
+                    action="/contact-success/"
+                    data-netlify="true"
+                    data-netlify-recaptcha="true"
+                    onSubmit={this.handleSubmit} >
                     <input type="hidden" name="form-name" value="callback" />
-                    {/* <p hidden>
-                    <label>
-                        Don’t fill this out:{" "}
-                        <input name="bot-field"/>
-                    </label>
-                    </p> */}
                     <p>
                         <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" value={name} onChange={this.handleChange} />
+                        <input type="text" name="name" id="name" onChange={this.handleChange} />
                     </p>
                     <p>
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" value={email} onChange={this.handleChange} />
+                        <input type="text" name="email" id="email" onChange={this.handleChange} />
                     </p>
                     <p>
                         <label htmlFor="firm">Firma</label>
-                        <input type="text" name="firm" id="firm" value={firm} onChange={this.handleChange} />
+                        <input type="text" name="firm" id="firm" onChange={this.handleChange} />
                     </p>
                     <p>
                         <label htmlFor="phone">Telefon*</label>
-                        <input type="text" name="phone" id="phone" required value={phone} onChange={this.handleChange}/>
-                        {/* <label className="invalid-feedback" /> */}
+                        <input type="text" name="phone" id="phone" required onChange={this.handleChange}/>
                     </p>
                     <p className="full-width">
                         <label htmlFor="message">Ihre Nachricht an uns</label>
-                        <textarea name="message" id="message" rows="12" value={message} onChange={this.handleChange} />
+                        <textarea name="message" id="message" rows="12" onChange={this.handleChange} />
                         
                     </p>
-                    <div className="g-recaptcha" data-sitekey="6LeslmMUAAAAAA8r8zMtm2Osz5Nsvsqfdn7xh3QI"/>
+                    <Recaptcha
+                        ref="recaptcha"
+                        sitekey={RECAPTCHA_KEY}
+                        onChange={this.handleRecaptcha}
+                    />
                     <p className="full-width">
                         <input type="submit" value="Rückruf Wunsch absenden" />
                     </p>
--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { navigateTo } from 'gatsby-link';
 import './contact-form.css';
-import Form from '../form/forms.module';
+import Recaptcha from "react-google-recaptcha";
 import '../form/forms.css';
+
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 
 const encode = (data) => {
     return Object.keys(data)
@@ -10,32 +12,45 @@ const encode = (data) => {
         .join("&");
   }
 
-  
+
 class ContactForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { formSend: false, name: "", email: "", message: "", phone: "", firm: "" };
+        this.state = { };
     }
 
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+      };
+    
+    handleRecaptcha = value => {
+        this.setState({ "g-recaptcha-response": value });
+    };
+    
     handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+        if (!this.state['g-recaptcha-response']) {
+            alert('recaptcha nicht bestätigt')
+            return;
+        }
+        const form = e.target;
         fetch("/", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", ...this.state })
+          body: encode({
+            "form-name": form.getAttribute("name"),
+            ...this.state
+          })
         })
-          .then(() => navigateTo('/contact-success'))
-          .catch(error => console.log(error));
-  
-        e.preventDefault();
-      };
-  
-    handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
+          .then(() => navigateTo(form.getAttribute("action")))
+          .catch(error => alert(error));
+    };
 
     render() {
         return (
             <div className="contact">
-                <Form name="contact" method="POST" action="/contact-success" submit={this.handleSubmit}>
+                <form name="contact" method="POST" action="/contact-success" onSubmit={this.handleSubmit}>
                     <input type="hidden" name="form-name" value="contact" />
                     <p hidden>
                     <label>
@@ -45,13 +60,11 @@ class ContactForm extends Component {
                     </p>
                     <p>
                         <label htmlFor="name">Name*</label>
-                        <input type="text" name="name" id="name" required/>
-                        <label className="invalid-feedback" />
+                        <input type="text" name="name" id="name" required onChange={this.handleChange}/>
                     </p>
                     <p>
                         <label htmlFor="email">Email*</label>
-                        <input type="text" name="email" id="email" required/>
-                        <label className="invalid-feedback" />
+                        <input type="text" name="email" id="email" required onChange={this.handleChange}/>
                     </p>
                     <p>
                         <label htmlFor="firm">Firma</label>
@@ -63,14 +76,17 @@ class ContactForm extends Component {
                     </p>
                     <p className="full-width">
                         <label htmlFor="message">Ihre Nachricht an uns*</label>
-                        <textarea name="message" id="message" rows="12" required/>
-                        <label className="invalid-feedback" />
+                        <textarea name="message" id="message" rows="12" required onChange={this.handleChange}/>
                     </p>
-                    <div netlify-recaptcha="true" />
+                    <Recaptcha
+                        ref="recaptcha"
+                        sitekey={RECAPTCHA_KEY}
+                        onChange={this.handleRecaptcha}
+                    />
                     <p className="full-width">
                         <input type="submit" value="Nachricht absenden" />
                     </p>
-                </Form>
+                </form>
             </div>
         )
     }
