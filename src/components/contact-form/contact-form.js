bf65d786c12eb006a10ed55caf5c32421bac57f4
Martin Köhn <m.koehn@mip-consult.de>
2018-07-11 13:53:17 +0000

    update from gitlab

--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -1,94 +1,125 @@
-import React, { Component } from 'react';
-import { navigateTo } from 'gatsby-link';
-import './contact-form.css';
-import Recaptcha from "react-google-recaptcha";
-import '../form/forms.css';
+import React, { Component } from 'react'
+import { navigateTo } from 'gatsby-link'
+import './contact-form.css'
+import Recaptcha from 'react-google-recaptcha'
+import '../form/forms.css'
 
-const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY
 
-const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
-
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
 class ContactForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { };
-    }
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-      };
-    
-    handleRecaptcha = value => {
-        this.setState({ "g-recaptcha-response": value });
-    };
-    
-    handleSubmit = e => {
-        e.preventDefault();
-        console.log(this.state);
-        if (!this.state['g-recaptcha-response']) {
-            alert('recaptcha nicht bestätigt')
-            return;
-        }
-        const form = e.target;
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({
-            "form-name": form.getAttribute("name"),
-            ...this.state
-          })
-        })
-          .then(() => navigateTo(form.getAttribute("action")))
-          .catch(error => alert(error));
-    };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
-    render() {
-        return (
-            <div className="contact">
-                <form name="contact" method="POST" action="/contact-success" onSubmit={this.handleSubmit}>
-                    <input type="hidden" name="form-name" value="contact" />
-                    <p hidden>
-                    <label>
-                        Don’t fill this out:{" "}
-                        <input name="bot-field"/>
-                    </label>
-                    </p>
-                    <p>
-                        <label htmlFor="name">Name*</label>
-                        <input type="text" name="name" id="name" required onChange={this.handleChange}/>
-                    </p>
-                    <p>
-                        <label htmlFor="email">Email*</label>
-                        <input type="text" name="email" id="email" required onChange={this.handleChange}/>
-                    </p>
-                    <p>
-                        <label htmlFor="firm">Firma</label>
-                        <input type="text" name="firm" id="firm" onChange={this.handleChange}/>
-                    </p>
-                    <p>
-                        <label htmlFor="phone">Telefon</label>
-                        <input type="text" name="phone" id="phone" onChange={this.handleChange}/>
-                    </p>
-                    <p className="full-width">
-                        <label htmlFor="message">Ihre Nachricht an uns*</label>
-                        <textarea name="message" id="message" rows="12" required onChange={this.handleChange}/>
-                    </p>
-                    <Recaptcha
-                        ref="recaptcha"
-                        sitekey={RECAPTCHA_KEY}
-                        onChange={this.handleRecaptcha}
-                    />
-                    <p className="full-width">
-                        <input type="submit" value="Nachricht absenden" />
-                    </p>
-                </form>
-            </div>
-        )
+  handleRecaptcha = value => {
+    this.setState({ 'g-recaptcha-response': value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state)
+    if (!this.state['g-recaptcha-response']) {
+      alert('recaptcha nicht bestätigt')
+      return
     }
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute('action')))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <div className="contact">
+        <form
+          name="contact"
+          method="POST"
+          action="/contact-success"
+          onSubmit={this.handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" />
+            </label>
+          </p>
+          <p>
+            <label htmlFor="name">Name*</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label htmlFor="email">Email*</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              required
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label htmlFor="firm">Firma</label>
+            <input
+              type="text"
+              name="firm"
+              id="firm"
+              onChange={this.handleChange}
+            />
+          </p>
+          <p>
+            <label htmlFor="phone">Telefon</label>
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              onChange={this.handleChange}
+            />
+          </p>
+          <p className="full-width">
+            <label htmlFor="message">Ihre Nachricht an uns*</label>
+            <textarea
+              name="message"
+              id="message"
+              rows="12"
+              required
+              onChange={this.handleChange}
+            />
+          </p>
+          <Recaptcha
+            ref="recaptcha"
+            sitekey={RECAPTCHA_KEY}
+            onChange={this.handleRecaptcha}
+          />
+          <p className="full-width">
+            <input type="submit" value="Nachricht absenden" />
+          </p>
+        </form>
+      </div>
+    )
+  }
 }
-export default ContactForm;
+export default ContactForm
--- src/pages/software-dev.js
+++ src/pages/software-dev.js
@@ -1,97 +1,145 @@
 import React from 'react'
 
-import Greybox from '../components/grey-box/grey-box.module';
-import BlueBox from '../components/blue-box/blue-box';
-import Whitebox from '../components/white-box/white-box.module';
+import Greybox from '../components/grey-box/grey-box.module'
+import BlueBox from '../components/blue-box/blue-box'
+import Whitebox from '../components/white-box/white-box.module'
 
-import CodeWhite from '../images/code-icon_weiß.png';
-import RocketWhite from '../images/raketen-icon_weiß.png';
-import ConceptWhite from '../images/konzept-icon_weiß.png';
-import MagnifierWhite from '../images/lupe-icon_weiß.png';
-import IconBall from '../components/icon-ball/icon-ball';
-import Golang from '../images/golang-blue.svg';
-import Angular from '../images/angular.svg';
-import MongoDB from '../images/mongodb.png';
-import NodeJs from '../images/nodejs.png';
-import Firebase from '../images/firebase-logo.svg';
-import Loopback from '../images/loopback-logo.png';
-import Docker from '../images/docker_vertical.png';
+import CodeWhite from '../images/code-icon_weiß.png'
+import RocketWhite from '../images/raketen-icon_weiß.png'
+import ConceptWhite from '../images/konzept-icon_weiß.png'
+import MagnifierWhite from '../images/lupe-icon_weiß.png'
+import IconBall from '../components/icon-ball/icon-ball'
+import Golang from '../images/golang-blue.svg'
+import Angular from '../images/angular.svg'
+import MongoDB from '../images/mongodb.png'
+import NodeJs from '../images/nodejs.png'
+import Firebase from '../images/firebase-logo.svg'
+import Loopback from '../images/loopback-logo.png'
+import Docker from '../images/docker_vertical.png'
 
 const SoftwareDev = () => (
-<div>
-  <Whitebox>
-    <h2>Vorgehen</h2>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Erat imperdiet sed euismod nisi porta lorem mollis. Non enim praesent elementum facilisis leo vel fringilla. Quisque non tellus orci ac auctor. Turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet. Amet facilisis magna etiam tempor. Mauris ultrices eros in cursus turpis massa tincidunt. Sit amet cursus sit amet dictum sit amet justo. Erat nam at lectus urna duis convallis. Ultricies tristique nulla aliquet enim tortor. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Ultrices dui sapien eget mi proin. Nisl tincidunt eget nullam non nisi. Vulputate odio ut enim blandit volutpat. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.  
-    </p>
-    <br />
-    <div style={{textAlign: 'center'}}>
-      <div style={{display: 'inline-block'}}>
-        <IconBall bgColor='#13597C' color='#fff' imgSrc={ConceptWhite}>
-          Konzept
-        </IconBall>
-      </div>
-    </div>
-    <div style={{textAlign: 'center'}}>
-      <div style={{display: 'inline-block'}}>
-        <IconBall bgColor='#13597C' color='#fff' imgSrc={RocketWhite}>
-          Veröffentlichen
-        </IconBall>
-      </div>
-      <div style={{display: 'inline-block', width: '12.5rem'}} />
-      <div style={{display: 'inline-block'}}>
-        <IconBall bgColor='#13597C' color='#fff' imgSrc={CodeWhite}>
-          Entwicklung
-        </IconBall>
-      </div>
-    </div>
-    <div style={{textAlign: 'center'}}>
-      <div style={{display: 'inline-block'}}>
-        <IconBall bgColor='#13597C' color='#fff' imgSrc={MagnifierWhite}>
-          Testen
-        </IconBall>
-      </div>
-    </div>
-  </Whitebox>
-  <Greybox>
-    <h2>Web-Anwendungen</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Erat imperdiet sed euismod nisi porta lorem mollis. Non enim praesent elementum facilisis leo vel fringilla. Quisque non tellus orci ac auctor. Turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet. Amet facilisis magna etiam tempor. Mauris ultrices eros in cursus turpis massa tincidunt. Sit amet cursus sit amet dictum sit amet justo. Erat nam at lectus urna duis convallis. Ultricies tristique nulla aliquet enim tortor. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Ultrices dui sapien eget mi proin. Nisl tincidunt eget nullam non nisi. Vulputate odio ut enim blandit volutpat. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.
-    </p>
-  </Greybox>
-  <BlueBox>
-    <h2>Migration von alten Systemen</h2>
-    <p style={{color: '#fff'}}>
-    Bei der Umstellung auf ein neues System möchten Sie natürlich nicht auf Ihren bisherigen Datensätze verzichten. Auch hier sind wir gerne behilflich und schaffen bei Beauftragung eine Schnittstelle zwischen Ihren alten Daten und der für Sie entwickelten Software.
-    </p>
-  </BlueBox>
-  <Whitebox>
-    <h2>Technologien</h2>
-      <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
-        <div style={{margin: '0 1rem'}}>
-          <a href="https://angular.io/"><img src={Angular}   style={{height: '200px'}}/></a>
-        </div>
-        <div style={{margin: '2rem 1rem'}}>
-        <a href="https://firebase.google.com/"><img src={Firebase}   style={{maxHeight: '200px'}}/></a>
-        </div>
-        <div style={{margin: '0 1rem'}}>
-        <a href="https://golang.org/"><img src={Golang}   style={{height: '200px'}}/></a>
-        </div>
-        <div style={{margin: '0 1rem'}}>
-        <a href="https://nodejs.org/"><img src={NodeJs}   style={{height: '200px'}}/></a>
-        </div>
-        <div style={{margin: '0 1rem'}}>
-        <a href="https://loopback.io/"><img src={Loopback}   style={{maxHeight: '200px'}}/></a>
-        </div>
-        <div style={{margin: '0 1rem'}}>
-        <a href="https://www.docker.com/"><img src={Docker}   style={{maxHeight: '200px'}}/></a>
-        </div>
-        <div style={{margin: '0 1rem'}}>
-        <a href="https://www.mongodb.com/"><img src={MongoDB}   style={{maxHeight: '150px'}}/></a>
+  <div>
+    <Whitebox>
+      <h2>Vorgehen</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Erat imperdiet sed
+        euismod nisi porta lorem mollis. Non enim praesent elementum facilisis
+        leo vel fringilla. Quisque non tellus orci ac auctor. Turpis nunc eget
+        lorem dolor sed viverra ipsum nunc aliquet. Amet facilisis magna etiam
+        tempor. Mauris ultrices eros in cursus turpis massa tincidunt. Sit amet
+        cursus sit amet dictum sit amet justo. Erat nam at lectus urna duis
+        convallis. Ultricies tristique nulla aliquet enim tortor. Cursus in hac
+        habitasse platea dictumst quisque sagittis purus sit. Ultrices dui
+        sapien eget mi proin. Nisl tincidunt eget nullam non nisi. Vulputate
+        odio ut enim blandit volutpat. Viverra tellus in hac habitasse platea
+        dictumst vestibulum rhoncus. Leo integer malesuada nunc vel risus
+        commodo viverra maecenas accumsan.
+      </p>
+      <br />
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ display: 'inline-block' }}>
+          <IconBall bgColor="#13597C" color="#fff" imgSrc={ConceptWhite}>
+            Konzept
+          </IconBall>
         </div>
       </div>
-  </Whitebox>
-</div>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ display: 'inline-block' }}>
+          <IconBall bgColor="#13597C" color="#fff" imgSrc={RocketWhite}>
+            Veröffentlichen
+          </IconBall>
+        </div>
+        <div style={{ display: 'inline-block', width: '12.5rem' }} />
+        <div style={{ display: 'inline-block' }}>
+          <IconBall bgColor="#13597C" color="#fff" imgSrc={CodeWhite}>
+            Entwicklung
+          </IconBall>
+        </div>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ display: 'inline-block' }}>
+          <IconBall bgColor="#13597C" color="#fff" imgSrc={MagnifierWhite}>
+            Testen
+          </IconBall>
+        </div>
+      </div>
+    </Whitebox>
+    <Greybox>
+      <h2>Web-Anwendungen</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Erat imperdiet sed
+        euismod nisi porta lorem mollis. Non enim praesent elementum facilisis
+        leo vel fringilla. Quisque non tellus orci ac auctor. Turpis nunc eget
+        lorem dolor sed viverra ipsum nunc aliquet. Amet facilisis magna etiam
+        tempor. Mauris ultrices eros in cursus turpis massa tincidunt. Sit amet
+        cursus sit amet dictum sit amet justo. Erat nam at lectus urna duis
+        convallis. Ultricies tristique nulla aliquet enim tortor. Cursus in hac
+        habitasse platea dictumst quisque sagittis purus sit. Ultrices dui
+        sapien eget mi proin. Nisl tincidunt eget nullam non nisi. Vulputate
+        odio ut enim blandit volutpat. Viverra tellus in hac habitasse platea
+        dictumst vestibulum rhoncus. Leo integer malesuada nunc vel risus
+        commodo viverra maecenas accumsan.
+      </p>
+    </Greybox>
+    <BlueBox>
+      <h2>Migration von alten Systemen</h2>
+      <p style={{ color: '#fff' }}>
+        Bei der Umstellung auf ein neues System möchten Sie natürlich nicht auf
+        Ihren bisherigen Datensätze verzichten. Auch hier sind wir gerne
+        behilflich und schaffen bei Beauftragung eine Schnittstelle zwischen
+        Ihren alten Daten und der für Sie entwickelten Software.
+      </p>
+    </BlueBox>
+    <Whitebox>
+      <h2>Technologien</h2>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+        }}
+      >
+        <div style={{ margin: '0 1rem' }}>
+          <a href="https://angular.io/">
+            <img src={Angular} style={{ height: '200px' }} />
+          </a>
+        </div>
+        <div style={{ margin: '2rem 1rem' }}>
+          <a href="https://firebase.google.com/">
+            <img src={Firebase} style={{ maxHeight: '200px' }} />
+          </a>
+        </div>
+        <div style={{ margin: '0 1rem' }}>
+          <a href="https://golang.org/">
+            <img src={Golang} style={{ height: '200px' }} />
+          </a>
+        </div>
+        <div style={{ margin: '0 1rem' }}>
+          <a href="https://nodejs.org/">
+            <img src={NodeJs} style={{ height: '200px' }} />
+          </a>
+        </div>
+        <div style={{ margin: '0 1rem' }}>
+          <a href="https://loopback.io/">
+            <img src={Loopback} style={{ maxHeight: '200px' }} />
+          </a>
+        </div>
+        <div style={{ margin: '0 1rem' }}>
+          <a href="https://www.docker.com/">
+            <img src={Docker} style={{ maxHeight: '200px' }} />
+          </a>
+        </div>
+        <div style={{ margin: '0 1rem' }}>
+          <a href="https://www.mongodb.com/">
+            <img src={MongoDB} style={{ maxHeight: '150px' }} />
+          </a>
+        </div>
+      </div>
+    </Whitebox>
+  </div>
 )
 
-export default SoftwareDev;
+export default SoftwareDev
