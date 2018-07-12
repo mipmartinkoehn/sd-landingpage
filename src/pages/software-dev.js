1bae128c647a9d41bf7b738f78e983ac0193d86d
Martin Köhn <m.koehn@mip-consult.de>
2018-07-12 08:39:34 +0000

    update from gitlab

--- src/pages/software-dev.js
+++ src/pages/software-dev.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import Greybox from '../components/grey-box/grey-box.module';
-import BlueBox from '../components/blue-box/blue-box.module';
-import Whitebox from '../components/white-box/white-box.module';
+import Greybox from '../components/grey-box/grey-box.module'
+import BlueBox from '../components/blue-box/blue-box.module'
+import Whitebox from '../components/white-box/white-box.module'
 
 import CodeWhite from '../images/code-icon_weiß.png'
 import RocketWhite from '../images/raketen-icon_weiß.png'
@@ -85,11 +85,63 @@ const SoftwareDev = () => (
     </Greybox>
     <BlueBox>
       <h2>Migration von alten Systemen</h2>
-      <p style={{ color: '#fff' }}>
-        Bei der Umstellung auf ein neues System möchten Sie natürlich nicht auf
-        Ihren bisherigen Datensätze verzichten. Auch hier sind wir gerne
-        behilflich und schaffen bei Beauftragung eine Schnittstelle zwischen
-        Ihren alten Daten und der für Sie entwickelten Software.
+      <p>
+        Heute ändern sich die Anforderungen an Software schneller als je zuvor.
+        Dies erfordert eine der heutigen Zeit angemessene Architektur, die die
+        notwendige Agilität ermöglicht um Schritt zu halten mit ständig
+        wechselnden Anforderungen.
+      </p>
+      <p>
+        Ungewollte Abhängigkeiten, die im Laufe der Zeit gewachsen sind, lassen
+        die Architektur wie einen "Big Ball of Mud (BBoM)" aussehen. Solche
+        Monolithen weisen eine stark erhöhte Komplexität auf. Dies verringert
+        die Produktivität aufgrund von Koordinierungsaufwänden zwischen
+        verschiedenen Teams, komplizierte Releases und schlechte Wartbarkeit.
+        Als Gegenmaßnahme haben Unternehmen agile Methoden wie Scrum eingeführt,
+        um ihre Softwareentwicklungsprozesse zu optimieren. Bei der Agilität in
+        den Prozessen kann auf technischer Ebene aber oft nicht mitgehalten
+        werden. Daher empfiehlt es sich auch auf technischer Ebene die
+        Software-Archtiktur auf ein agileres Modell umzustellen.
+      </p>
+      <p>
+        Ein geeignetes Architekturmuster um komplexe Anwendungen in kleinere
+        Prozesse runter zu brechen und unabhängig von einander zu Entwickeln zu
+        zu Veröffentlichen sind Microservices. Microservices haben typischerweise die folgenden Eigenschaften:
+      </p>
+      <ul>
+        <li>Die Services können einfach ersetzt werden.</li>
+        <li>Die Dienste haben eine einzige Geschäftsfunktion.</li>
+        <li>Der Nutzen für den Benutzer steht im Mittelpunkt.</li>
+        <li>Ein Microservice wird nur von einem Team entwickelt.</li>
+        <li>Die Schnittstellen verstecken Implementierungsdetails.</li>
+        <li>Microservices werden gegenüber anderen Services isoliert.</li>
+        <li>Wie alle Services müssen auch Microservices sicher sein.</li>
+      </ul>
+      <p>
+        Von den alten monolitischen System hin zu einer agilen
+        Microservice-Architektur zu wechseln ist kein Prozess, der über Nacht
+        abläuft. Man muss genau abwägen wann und welche Teile der bisherigen
+        Archtiktur in kleinere Applikationen (Microservices) aufgesplittet
+        werden können und wie diese wieder in die Gesamtarchitektur integriert
+        werden.
+      </p>
+      <p>
+        Eine neue Architektur bringt auch andere Seiteneffekte mit sich, die bei
+        einer Umstellung zu betrachten sind. Folgende Fragestellungen tauchen
+        dabei immer wieder auf:{' '}
+      </p>
+      <ul>
+        <li>Wie werden Microservice deployed?</li>
+        <li>
+          Wie behalte ich den Überblick über die wachsende Service Struktur?
+        </li>
+        <li>Wie kommunizieren die verschiedenen Services miteinander?</li>
+      </ul>
+      <p>
+        Wir helfen Ihnen diese Fragen individuell zu beantworten, um eine
+        möglichst reibungslose Migration von Ihrer monolitischen
+        System-Architektur zu einer flexiblen Microservice Architektur zu
+        schaffen.
       </p>
     </BlueBox>
     <Whitebox>
